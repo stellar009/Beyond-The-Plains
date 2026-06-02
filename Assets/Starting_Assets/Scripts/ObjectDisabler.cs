b505b05edba9040fc21fbc3725c0e883b47aa318
stellar6495ED <stellarblue87@gmail.com>
2026-06-02 11:31:31 +0530

    Some minor modifications

---
+++ Assets/Starting_Assets/Scripts/ObjectDisabler.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class ObjectDisabler : MonoBehaviour
+{
+    [Header("Settings")]
+    [Tooltip("Add only objects which needs to disable when game starts")]
+    public GameObject[] objects;
+
+    private int objectsSize = 0;
+
+    private void Start()
+    {
+        objectsSize = objects.Length;
+
+        for (int i = 0; i < objectsSize; i++)
+        {
+            if (!objects[i].activeInHierarchy)
+            {
+                return;
+            }
+            else
+            {
+                objects[i].SetActive(false);
+            }
+        }
+    }
+}
