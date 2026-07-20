b3f34736f7a5b4b86bd916e54854ba39eea23f3e
stellar6495ED <stellarblue87@gmail.com>
2026-07-20 09:25:09 +0530

    Minor Modifications

--- Assets/GlobalAssets/Scripts_Global/GlobalGameQualitySettings.cs
+++ Assets/GlobalAssets/Scripts_Global/GlobalGameQualitySettings.cs
@@ -6,10 +6,16 @@ public class GlobalGameQualitySettings : MonoBehaviour
     private void Start()
     {
         EnableSRPBatching(true);
+        UseVSync();
     }
 
     void EnableSRPBatching(bool state)
     {
         GraphicsSettings.useScriptableRenderPipelineBatching = state;
+    }
+
+    public void UseVSync()
+    {
+        QualitySettings.vSyncCount = 1;
     }
 }
